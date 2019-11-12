0f7681a3c5dacfed6f18d1cb82da65f8a3cde3c8
Lauren Jahera <lauren.jahera@gmail.com>
2019-11-12 10:08:34 -0500

    initial commit

---
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -0,0 +1,26 @@
+package org.launchcode.controllers;
+
+import org.launchcode.models.Category;
+import org.launchcode.models.data.CategoryDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryDao categoryDao;
+
+    @RequestMapping("")
+    public class index(Model model; String Categories){
+
+        model.addAttribute("categoryList", categoryDao.findAll());
+        model.addAttribute("title", Categories);
+
+        return "category/index";
+    }
+
+}
