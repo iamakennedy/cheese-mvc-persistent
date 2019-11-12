987e97e1e60035f0d5f7ee5904b880389cc215c2
Lauren Jahera <lauren.jahera@gmail.com>
2019-11-12 13:16:13 -0500

    add MenuDao

--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -1,13 +1,16 @@
 package org.launchcode.models;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Category {
+public class Menu {
 
     @Id
     @GeneratedValue
@@ -17,19 +20,22 @@ public class Category {
     @Size(min=3, max=15)
     private String name;
 
-    @OneToMany
-    @JoinColumn(name = "category_id")
+    @ManyToMany
     private List<Cheese> cheeses = new ArrayList<>();
 
-    public Category() {
+    public Menu(String name) {
+        this.name = name;
     }
 
-    public Category(String name) {
-        this.name = name;
+    public Menu() {
     }
 
     public int getId() {
         return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -39,5 +45,14 @@ public class Category {
     public void setName(String name) {
         this.name = name;
     }
+
+    public List<Cheese> getCheeses() {
+        return cheeses;
+    }
+
+    public void addItem(Cheese item) {
+        cheeses.add(item);
+    }
+
 
 }
---
+++ src/main/java/org/launchcode/models/data/MenuDao.java
@@ -0,0 +1,10 @@
+package org.launchcode.models.data;
+
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
+public interface MenuDao {
+}
